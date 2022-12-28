ec3eaa0745fdb417199a8055060a882fe6322563
saurav7632 <121542006+saurav7632@users.noreply.github.com>
2022-12-28 14:39:29 +0530

    remove cors

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express=require("express");
-const cors = require("cors");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const path=require("path")
